faa8054a527aec72477890424456ccbeab6aa8f9
developer-nieem <naimhossenwork@gmail.com>
2023-09-03 00:30:30 +0600

    complete showing note list data

--- src/components/YourList/YourList.jsx
+++ src/components/YourList/YourList.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { HiPencilAlt, HiTrash } from "react-icons/hi";
 
 const YourList = () => {
     const [lists , setLists] = useState([]);
@@ -14,9 +16,18 @@ const YourList = () => {
     return (
         <div>
             {
-                lists.map(item=> <div key={item._id}>
-                    
+                lists.map(item=> <div key={item._id} className=' border p-3 my-3 flex justify-between items-start'>
+                   
+                   <div className='space-y-2'>
+                   <h2 className='text-4xl font-semibold'>{item.title}</h2>
+                    <p>{item.details}</p>
+                    <p> <span className='font-semibold'>Your Note Status:</span> {item.status} </p>
+                   </div>
 
+                    <div className='flex gap-5'>
+                        <Link to={`/update-note/${item._id}`} className=' text-3xl'> <HiPencilAlt></HiPencilAlt> </Link>
+                        <button className='text-3xl' id={item._id} > <HiTrash></HiTrash> </button>
+                    </div>
                 </div> )
             }
         </div>
