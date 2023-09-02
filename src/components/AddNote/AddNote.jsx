e17fd48e529d635abbde479cfca5bb4c3ca8208e
developer-nieem <naimhossenwork@gmail.com>
2023-09-03 00:13:30 +0600

    complete add to note

--- src/components/AddNote/AddNote.jsx
+++ src/components/AddNote/AddNote.jsx
@@ -1,28 +1,78 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const AddNote = () => {
-    return (
+
+    const router = useNavigate()
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    const data = {
+      title: form.title.value,
+      details: form.details.value,
+      status: form.status.value,
+    };
+   
+
+    const res = fetch("http://localhost:3000/todo-list", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (res) {
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Saved Your Note',
+            timer: 1500
+          })
+
+          router('/')
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className="space-y-4 my-5">
         <div>
-            <form className='space-y-4 my-5'>
-                <div>
-              <input type="text" name='title' className='bg-slate-200 p-3 w-full rounded-md'  placeholder='Title'/>
-                </div>
-                <div>
-                <textarea className='bg-slate-200 p-3 w-full rounded-md' placeholder='Details' rows='5' name='details'></textarea>
-                </div>
-                <div className='flex gap-2'>
-                    <h2 className='font-semibold'>Work Status : </h2>
-                <label>
-                <input type="checkbox"  value='running' name='status'/> 
-                <span>Running</span>
-                </label>
-                </div>
-               <div className='text-center'>
-               <button className='bg-black p-3 rounded-sm text-white hover:bg-slate-600 '>Add To List</button>
-               </div>
-            </form>
+          <input
+            type="text"
+            name="title"
+            className="bg-slate-200 p-3 w-full rounded-md"
+            placeholder="Title"
+            required
+          />
         </div>
-    );
+        <div>
+          <textarea
+            className="bg-slate-200 p-3 w-full rounded-md"
+            placeholder="Details"
+            rows="5"
+            name="details"
+            required
+          ></textarea>
+        </div>
+        <div className="flex gap-2">
+          <h2 className="font-semibold">Work Status : </h2>
+          <label>
+            <input type="checkbox" value="running" name="status" required />
+            <span>Running</span>
+          </label>
+        </div>
+        <div className="text-center">
+          <button className="bg-black p-3 rounded-sm text-white hover:bg-slate-600 ">
+            Add To List
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AddNote;
--- src/components/YourList/YourList.jsx
+++ src/components/YourList/YourList.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const YourList = () => {
+    const [lists , setLists] = useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:3000/todo-list')
+        .then(res => res.json())
+        .then(data => setLists(data))
+
+    }, [])
+
+    console.log(lists);
     return (
         <div>
-            
+            {
+                lists.map(item=> <div key={item._id}>
+                    
+
+                </div> )
+            }
         </div>
     );
 };
